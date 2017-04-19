15bca09e7ecaf960fe6f9c5de3638805ba3c6a13
Drm1804 <drm-a@yandex.ru>
2017-04-19 23:04:39 +0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import {MdButtonModule, MdCheckboxModule, MdGridListModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { TimeLineComponent } from './time-line/time-line.component';
-import { LineFillComponent } from './time-line/line-fill/line-fill.component';
+import {AppComponent} from './app.component';
+import {TimeLineComponent} from './time-line/time-line.component';
+import {LineFillComponent} from './time-line/line-fill/line-fill.component';
+
+const appRoutes: Routes = [
+    {path: 'timeline', component: TimeLineComponent},
+    {
+        path: '',
+        redirectTo: '/timeline',
+        pathMatch: 'full'
+    },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TimeLineComponent,
-    LineFillComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    BrowserAnimationsModule,
-    MdButtonModule,
-    MdGridListModule,
-    MdCheckboxModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TimeLineComponent,
+        LineFillComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdGridListModule,
+        MdCheckboxModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
